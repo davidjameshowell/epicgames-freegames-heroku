6ee68233e7576d87386178cf0fd23acf087231d7
davidjameshowell <david@davidjameshowell.com>
2020-12-20 08:20:06 +0000

    More build app image fixes.

--- deploy_update_app.sh
+++ deploy_update_app.sh
@@ -72,10 +72,10 @@ function build_image {
     cd ./${EPICGAMES_FREEGAMES_FOLDER}
     
     printf "Heroku uses random ports for assignment with httpd services. We are modifying the SERVER_PORT in entrypoint for startup."
-    sed_files '2 a export SERVER_PORT=\$PORT\n' ./${EPICGAMES_FREEGAMES_FOLDER}/entrypoint.sh
-    sed_files '3 a touch /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json' ./${EPICGAMES_FREEGAMES_FOLDER}/entrypoint.sh
-    sed_files '4 a redis-cli -u \$REDISTOGO_URL get EMAIL_COOKIE | base64 -d > /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json' ./${EPICGAMES_FREEGAMES_FOLDER}/entrypoint.sh
-    sed_files '$a echo $(cat /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json | base64) | redis-cli -u \$REDISTOGO_URL set -x EMAIL_COOKIE' ./${EPICGAMES_FREEGAMES_FOLDER}/entrypoint.sh
+    sed_files '2 a export SERVER_PORT=\$PORT\n' ./entrypoint.sh
+    sed_files '3 a touch /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json' ./entrypoint.sh
+    sed_files '4 a redis-cli -u \$REDISTOGO_URL get EMAIL_COOKIE | base64 -d > /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json' ./entrypoint.sh
+    sed_files '$a echo $(cat /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json | base64) | redis-cli -u \$REDISTOGO_URL set -x EMAIL_COOKIE' ./entrypoint.sh
 
     # Dockerfile manipulation to install redis
     sed_files 's/jq tzdata/jq tzdata redis/g' ./${EPICGAMES_FREEGAMES_FOLDER}/Dockerfile
