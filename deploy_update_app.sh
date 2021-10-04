97d07c94c57580aa88be7255a83dfb307e001679
David Howell <david@davidjameshowell.com>
2021-10-04 01:23:09 +0000

    Testing

--- deploy_update_app.sh
+++ deploy_update_app.sh
@@ -105,7 +105,7 @@ function build_image {
     printf "We are additionally adding logic to capture and set the Email Cookie for continued runs.\n"
     sed_files '2 a export SERVER_PORT=\$PORT\n' ./entrypoint.sh
     #sed_files '3 a if ! [ -z $(redis-cli -u \$REDISTOGO_URL get EMAIL_COOKIE) ]; then mkdir -p /usr/app/config && touch /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json && redis-cli -u \$REDISTOGO_URL get EMAIL_COOKIE | base64 -d > /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json; fi' ./entrypoint.sh
-    sed_files '4 a if ! [ -z $(redis-cli -u \$REDISTOGO_URL get APP_CONFIG) ]; then mkdir -p /usr/app/config && touch /usr/app/config/config.json && redis-cli -u \$REDISTOGO_URL get APP_CONFIG | base64 -d > /usr/app/config/config.json; fi' ./entrypoint.sh
+    sed_files '4 a if ! [ -z $(redis-cli -u \$REDISTOGO_URL get APP_CONFIG) ]; then echo "Adding App Config from redis url \${REDISTOGO_URL}" && mkdir -p /usr/app/config && touch /usr/app/config/config.json && redis-cli -u \$REDISTOGO_URL get APP_CONFIG | base64 -d > /usr/app/config/config.json; fi' ./entrypoint.sh
     sed_files '$a if [ -s /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json ]; then echo $(cat /usr/app/config/'${EMAIL_ADDRESS}'-cookies.json | base64) | redis-cli -u \$REDISTOGO_URL -x set EMAIL_COOKIE; fi' ./entrypoint.sh
 
     # Dockerfile manipulation to install redis
